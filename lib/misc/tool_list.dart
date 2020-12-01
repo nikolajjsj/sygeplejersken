2b9e0df6d49158e3e4fb0675f7194b15d1119eaf
Nikolaj Johannes Skole Jensen <10490273+nikolajjsj@users.noreply.github.com>
2020-12-01 13:58:03 +0100

    Added the widgets for each tool

--- lib/misc/tool_list.dart
+++ lib/misc/tool_list.dart
@@ -1,47 +1,54 @@
 import 'package:flutter/material.dart';
 import 'package:sygeplejersken/models/tool_model.dart';
+import 'package:sygeplejersken/screens/tools/bmi.dart';
+import 'package:sygeplejersken/screens/tools/braden.dart';
+import 'package:sygeplejersken/screens/tools/gcs.dart';
+import 'package:sygeplejersken/screens/tools/medicin.dart';
+import 'package:sygeplejersken/screens/tools/sonde.dart';
+import 'package:sygeplejersken/screens/tools/time.dart';
+import 'package:sygeplejersken/screens/tools/tobs.dart';
 
 final List<HealthTool> tools = <HealthTool>[
   HealthTool(
     title: 'BMI',
     description: 'Body Mass Index',
     imageAsset: 'assets/images/bmi.jpg',
-    tool: Material(),
+    tool: BmiTool(),
   ),
   HealthTool(
     title: 'Braden score',
     description: 'Vurdering af risiko for tryksår',
     imageAsset: 'assets/images/braden.jpg',
-    tool: Material(),
+    tool: BradenTool(),
   ),
   HealthTool(
     title: 'GCS',
     description: 'Glasgow Coma Scale',
     imageAsset: 'assets/images/gcs.jpg',
-    tool: Material(),
+    tool: GcsTool(),
   ),
   HealthTool(
     title: 'Medicinregning',
     description: 'Stofmængde, koncentrations, og volumenregning',
     imageAsset: 'assets/images/medicin.jpg',
-    tool: Material(),
+    tool: MedicinTool(),
   ),
   HealthTool(
     title: 'Sondeernæring',
     description: 'Beregning af væske- og sondeernæringsbehov',
     imageAsset: 'assets/images/sonde.jpg',
-    tool: Material(),
+    tool: SondeTool(),
   ),
   HealthTool(
     title: 'TIME - modellen',
     description: 'Vurdering af sår og sårbehandling',
     imageAsset: 'assets/images/time.jpg',
-    tool: Material(),
+    tool: TimeTool(),
   ),
   HealthTool(
     title: 'TOBS',
     description: 'Tidlig opsporing af begyndende sygdom',
     imageAsset: 'assets/images/tobs.jpg',
-    tool: Material(),
+    tool: TobsTool(),
   ),
 ];
